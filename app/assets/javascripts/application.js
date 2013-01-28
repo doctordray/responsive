6a20a16c56ac56a8ae76c438d953dbc418da2457
doctordray <acheam@gmail.com>
2013-01-28 16:19:30 +1100

    added new home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,8 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
+//= require vendor/modernizr-2.6.2-respond-1.1.0.min
 //= require jquery
 //= require jquery_ujs
+//= require plugins
 //= require_tree .
